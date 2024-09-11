9faa69b6464bcf46d1152088754c9b560c90d737
kkkiikkk <ruddnovskiy@gmail.com>
2024-09-11 12:24:46 +0300

    feat: add map, change create request to api module

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :tickets, only: [:create, :index, :show]
+  namespace :api do
+    resources :tickets, only: [:create]
+  end
+
+  resources :tickets, only: [:index, :show]
 end
